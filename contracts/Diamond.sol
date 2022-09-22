09a410df7657c93ef7631242b4813fa186079bf2
ukemeSkywalker <skywalkershadow9@gmail.com>
2022-09-22 13:24:51 +0100

    Implement AppStorage in Diamond.sol

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -11,9 +11,12 @@ pragma solidity ^0.8.0;
 import { LibDiamond } from "./libraries/LibDiamond.sol";
 import { IDiamondCut } from "./interfaces/IDiamondCut.sol";
 
+import "./libraries/LibERC721Storage.sol";
+
 contract Diamond {    
 
-    constructor(address _contractOwner, address _diamondCutFacet) payable {        
+    AppStorage internal x;
+    constructor(address _contractOwner, address _diamondCutFacet, string memory name, string memory symbol) payable {        
         LibDiamond.setContractOwner(_contractOwner);
 
         // Add the diamondCut external function from the diamondCutFacet
@@ -25,7 +28,11 @@ contract Diamond {
             action: IDiamondCut.FacetCutAction.Add, 
             functionSelectors: functionSelectors
         });
-        LibDiamond.diamondCut(cut, address(0), "");        
+        LibDiamond.diamondCut(cut, address(0), ""); 
+        x._name = name;
+        x._symbol = symbol;
+         
+              
     }
 
     // Find facet for function that is called and execute the
